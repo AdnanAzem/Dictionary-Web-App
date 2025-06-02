0997180edf90c87841e27126e3e2fac3e586745b
AdnanAzem <adnan.azem.250@gmail.com>
2025-06-02 07:11:12 +0300

    backend structure

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+// import wordRoutes from './routes/words';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+// app.use('/api/words', wordRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Dictionary API server is running ✅');
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
